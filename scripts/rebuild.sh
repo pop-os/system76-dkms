6e2d61a935fee34df52ce6120f4d32f19c0fec58
Jeremy Soller <jackpot51@gmail.com>
2018-02-15 10:01:39 -0700

    More complete rebuild process

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+dh clean
+make clean
+
+make
+make clean
+
 debuild -b -uc -us
+dh clean
+
 sudo dpkg -i ../system76-dkms_0.0.1_amd64.deb
-cat /var/lib/dkms/system76/0.0.1/build/make.log
-sudo modprobe -r system76
+
+sudo modprobe -r system76 || true
 sudo modprobe system76
+
 dmesg -w | grep system76
