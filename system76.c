546abd06708e3503e7f987b660fe789725ccf68b
Jeremy Soller <jeremy@system76.com>
2018-05-02 14:49:44 -0600

    Add galp3-b

--- system76.c
+++ system76.c
@@ -311,6 +311,7 @@ static int __init s76_dmi_matched(const struct dmi_system_id *id) {
 }
 
 static struct dmi_system_id s76_dmi_table[] __initdata = {
+	DMI_TABLE("galp3-b"),
 	DMI_TABLE("oryp3-jeremy"),
 	DMI_TABLE("oryp4"),
 	{}
